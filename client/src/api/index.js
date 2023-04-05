690c5e33ee897b122be55aa1f63b47f6967c3165
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-04-05 10:11:51 +0300

    add query-string

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
-const httpClient= axios.create({
-  baseURL:'http://localhost:3000/api',
+import qs from 'query-string';
+const httpClient = axios.create({
+  baseURL: 'http://localhost:3000/api',
 })
-export const getAllUsers=({limit,offset})=>httpClient.get('/users?limit=10&offset=299');
-export const postUser=(values)=>httpClient.post('/users',values);
+export const getAllUsers = (options = {}) => {
+  const defaultOption = {
+    limit: 5,
+    offset: 0
+  }
+  const readyOptions = {
+    ...defaultOption,
+    ...options,
+  }
+  return httpClient.get(`/users?${qs.stringify(readyOptions)}`);
+}
+export const postUser = (values) => httpClient.post('/users', values);
